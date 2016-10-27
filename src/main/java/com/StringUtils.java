c7fa61b1c371f8ccc9c25f241ee466739c6796ba
eric <zhangxin@zillionfortune.com>
2016-10-27 11:02:28 +0800

    git commit test

---
+++ src/main/java/com/StringUtils.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * String操作工具类
+ *
+ * @author
+ * @create 2016-10-27 11:00
+ */
+public class StringUtils {
+}
---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * 测试
+ *
+ * @author
+ * @create 2016-10-27 11:01
+ */
+public class Test {
+}
